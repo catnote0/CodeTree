f064aa76cb2dda1bbfa07c6d3aea8cf29d78f0e8
catnote0 <catnote1@gmail.com>
2025-05-07 16:19:36 +0900

    Title: 수열 조작, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250507/수열 조작/sequence-manipulation.cpp
+++ 250507/수열 조작/sequence-manipulation.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-queue<int> Q;
 int N;
-
 int main() {
     cin >> N;
-
-    for(int i = 1; i <= N; i++) Q.push(i);
-    for(int i = 1; i < N; i++) {
-        Q.pop();
-        Q.push(Q.front());
-        Q.pop();
-    }
-    cout << Q.front();
+    int n = 1;
+    while(n * 2 <= N) n *= 2;
+    if(n == N) cout << N;
+    else cout << (N - n) * 2;
     return 0;
 }
