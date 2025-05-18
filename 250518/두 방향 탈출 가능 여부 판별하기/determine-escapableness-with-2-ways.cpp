4c0b1c4837dcda99cd06b829a593ff42c160599f
catnote0 <catnote1@gmail.com>
2025-05-18 21:08:37 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1025ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250518/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int grid[110][110];
+void search(int x, int y) {
+    if(x == n - 1 && y == m - 1) {
+        printf("1");
+        exit(0);
+    }
+    if(grid[x + 1][y]) search(x + 1, y);
+    if(grid[x][y + 1]) search(x, y + 1);
+}
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    search(0, 0);
+    printf("0");
+    return 0;
+}
