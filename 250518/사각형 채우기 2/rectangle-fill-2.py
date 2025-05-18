0b23b31dcbd0c616a1c7e124abd586002069b6dc
catnote0 <catnote1@gmail.com>
2025-05-18 21:38:33 +0900

    Title: 사각형 채우기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250518/사각형 채우기 2/rectangle-fill-2.py
+++ 250518/사각형 채우기 2/rectangle-fill-2.py
@@ -1,4 +1,4 @@
 n = int(input())
 Result = [1, 1, 3]
-for i in range(3, n + 1): Result.append(Result[i - 2] * 2 + Result[i - 1])
+for i in range(3, n + 1): Result.append((Result[i - 2] * 2 + Result[i - 1]) % 10007)
 print(Result[n])
