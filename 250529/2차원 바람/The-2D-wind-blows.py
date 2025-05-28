5be402c78050bd231b3f9add38be888872021f9e
catnote0 <catnote1@gmail.com>
2025-05-29 01:40:47 +0900

    Title: 2차원 바람, Time: 79ms, Memory: 19MB, Status: Passed - Codetree

--- 250529/2차원 바람/The-2D-wind-blows.py
+++ 250529/2차원 바람/The-2D-wind-blows.py
@@ -1,6 +1,7 @@
 n, m, q = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
 winds = [tuple(map(int, input().split())) for _ in range(q)]
+near = [(-1, 0), (0, -1), (0, 1), (1, 0)]
 
 def is_valid(x, y):
     if x < 0 or x >= n or y < 0 or y >= m: return False
@@ -8,24 +9,27 @@ def is_valid(x, y):
 
 def blow(r1, c1, r2, c2):
     drc = [(0, 1)] * (c2 - c1) + [(1, 0)] * (r2 - r1) + [(0, -1)] * (c2 - c1) + [(-1, 0)] * (r2 - r1)
-    near = [(-1, 0), (0, -1), (0, 1), (1, 0)]
     x, y = r1, c1
     last_value, now_value = a[x][y], 0
-    for (dx, dy) in drc:
-        now_value = a[x + dx][y + dy]
-        a[x + dx][y + dy] = last_value
+    for dx, dy in drc:
+        nx, ny = x + dx, y + dy
+        now_value = a[nx][ny]
+        a[nx][ny] = last_value
         last_value = now_value
-        (x, y) = (x + dx, y + dy)
-    new_arr = [[0] * m for _ in range(n)]
-    for i in range(r1, r2 + 1):
-        for j in range(c1, c2 + 1):
+        x, y = nx, ny
+    h, w = r2 - r1 + 1, c2 - c1 + 1
+    new_arr = [[0] * w for _ in range(h)]
+    for di in range(h):
+        for dj in range(w):
+            i, j = r1 + di, c1 + dj
             Sum, Count = a[i][j], 1
-            for (dx, dy) in near:
-                if is_valid(i + dx, j + dy):
-                    Sum += a[i + dx][j + dy]
+            for dx, dy in near:
+                ni, nj = i + dx, j + dy
+                if 0 <= ni < n and 0 <= nj < m:
+                    Sum += a[ni][nj]
                     Count += 1
-            new_arr[i][j] = Sum // Count
-    for i in range(r1, r2 + 1): a[i][c1:(c2 + 1)] = new_arr[i][c1:(c2 + 1)]
+            new_arr[di][dj] = Sum // Count
+    for i in range(h): a[r1 + i][c1:c2 + 1] = new_arr[i]
 
                 
 for wind in winds: blow(wind[0] - 1, wind[1] - 1, wind[2] - 1, wind[3] - 1)
