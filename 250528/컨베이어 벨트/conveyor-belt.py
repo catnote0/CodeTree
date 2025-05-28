083ee84d30981847906b767e6ac828009c895763
catnote0 <catnote1@gmail.com>
2025-05-28 22:57:26 +0900

    Title: 컨베이어 벨트, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250528/컨베이어 벨트/conveyor-belt.py
+++ 250528/컨베이어 벨트/conveyor-belt.py
@@ -2,6 +2,7 @@ n, t = map(int, input().split())
 u = list(map(int, input().split()))
 d = list(map(int, input().split()))
 
+t %= (n * 2)
 r1 = " ".join(map(str, d[(n - t % n):n] + u[:(n - t % n)]))
 r2 = " ".join(map(str, u[(n - t % n):n] + d[:(n - t % n)]))
 print(r1 + "\n" + r2 if t < n else r2 + "\n" + r1)
