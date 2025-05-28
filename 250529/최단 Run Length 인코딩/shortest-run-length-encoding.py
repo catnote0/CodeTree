02436a23bc6f25b7fadd80764e6f202f9ef8772e
catnote0 <catnote1@gmail.com>
2025-05-29 02:57:49 +0900

    Title: 최단 Run Length 인코딩, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 250529/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,15 +1,3 @@
+from itertools import groupby
 A = input()
-result = len(A) * 2
-
-for i in range(len(A)):
-    combo = 1
-    length = 0
-    for j in range(len(A)):
-        if j < len(A) - 1 and A[j] == A[j + 1]: combo += 1
-        else:
-            length += len(str(combo)) + 1
-            combo = 1
-    result = min(result, length)
-    A = A[1:] + A[:1]
-
-print(result)
+print(min(sum(len(str(sum(1 for _ in grp))) + 1 for _, grp in groupby(A[i:] + A[:i])) for i in range(len(A))))
