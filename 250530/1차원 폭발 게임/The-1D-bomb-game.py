252f4270c13ca973b126035e173984a4df3e94b6
catnote0 <catnote1@gmail.com>
2025-05-30 00:03:11 +0900

    Title: 1차원 폭발 게임, Time: 1013ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250530/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+numbers = [int(input()) for _ in range(n)]
+
+while True:
+    combo = 1
+    for i in range(1, len(numbers)):
+        if numbers[i - 1] == numbers[i]: combo += 1
+        else:
+            if combo >= m: numbers[i - combo:i] = [-1] * combo
+            combo = 1
+    if combo >= m: numbers[len(numbers) - combo:len(numbers)] = [-1] * combo
+    if -1 not in numbers: break
+    while -1 in numbers: numbers.remove(-1)
+
+print(len(numbers))
+for num in numbers: print(num)
