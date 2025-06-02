d5d927059441dcc6e2f7b193951dec125e1f6c30
catnote0 <catnote1@gmail.com>
2025-06-03 03:25:34 +0900

    Title: 벽이 없는 충돌 실험, Time: 194ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 250603/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 250603/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,6 +1,6 @@
 T = int(input())
 drc = {'L': (-1, 0), 'R': (1, 0), 'U': (0, 1), 'D': (0, -1)}
-BOUNDARY = 2000
+strong_bead = {}
 
 for _ in range(T):
     N = int(input())
@@ -12,17 +12,21 @@ for _ in range(T):
         y.append(int(yi) * 2)
         w.append(int(wi))
         d.append(di)
-
+    
+    min_x, max_x, min_y, max_y = min(x), max(x), min(y), max(y)
     live = [True] * N
     t = 0
     last_collide = -1
+    alive_bead = N
 
     while True:
-        if not sum(live): break
+        if alive_bead <= 1: break
         t += 1
-        strong_bead = {}
+        strong_bead.clear()
         for i in range(N):
-            if x[i] < -BOUNDARY or BOUNDARY < x[i] or y[i] < -BOUNDARY or BOUNDARY < y[i]: live[i] = False
+            if x[i] < min_x or max_x < x[i] or y[i] < min_y or max_y < y[i]:
+                live[i] = False
+                alive_bead -= 1
             if not live[i]: continue
             x[i], y[i] = x[i] + drc[d[i]][0], y[i] + drc[d[i]][1]
             pri = w[i] * 1000 + i;
@@ -34,6 +38,7 @@ for _ in range(T):
                     live[now_pri % 1000] = False
                     strong_bead[xy] = pri
                 last_collide = t
+                alive_bead -= 1
             else: strong_bead[xy] = pri
 
     print(last_collide)
