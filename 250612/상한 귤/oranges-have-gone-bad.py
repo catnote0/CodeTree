32a8219841f79f73425fa1255f07465ff1d40358
catnote0 <catnote1@gmail.com>
2025-06-12 01:21:49 +0900

    Title: 상한 귤, Time: 100ms, Memory: 20MB, Status: Passed - Codetree

---
+++ 250612/상한 귤/oranges-have-gone-bad.py
@@ -0,0 +1,33 @@
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+drc = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+MAX_DIST = 0x7FFFFFFF
+Distance = [[MAX_DIST] * n for _ in range(n)]
+Queue = []
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 2:
+            Queue.append([i, j])
+            Distance[i][j] = 0
+
+T = 0
+while True:
+    T += 1
+    tmpQueue = []
+    for x, y in Queue:
+        for dx, dy in drc:
+            nx, ny = x + dx, y + dy
+            if nx < 0 or n <= nx or ny < 0 or n <= ny: continue
+            if grid[nx][ny] == 0: continue
+            if T < Distance[nx][ny]:
+                Distance[nx][ny] = T
+                tmpQueue.append([nx, ny])
+    if not tmpQueue: break
+    Queue = tmpQueue
+
+for i in range(n):
+    row = []
+    for j in range(n):
+        if grid[i][j] == 0: row.append(-1)
+        else: row.append(-2 if Distance[i][j] == MAX_DIST else Distance[i][j])
+    print(*row)
