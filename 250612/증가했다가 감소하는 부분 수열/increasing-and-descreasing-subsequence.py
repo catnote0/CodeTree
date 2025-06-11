bb79667f66ac2f599f03b0e5bc4329cb188acf0b
catnote0 <catnote1@gmail.com>
2025-06-12 03:36:50 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250612/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
+++ 250612/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -12,9 +12,10 @@ for i in range(n):
 for i in range(n - 1, -1, -1):
     max_value = 0
     for j in range(n - 1, i, -1):
-        if sequence[j] < sequence[i]: max_value = max(max_value, dp[j])
+        if sequence[j] < sequence[i]: max_value = max(max_value, dp_rev[j])
     dp_rev[i] = max_value + 1
 
 result = 0
 for i in range(n): result = max(result, dp[i] + dp_rev[i] - 1)
+
 print(result)
