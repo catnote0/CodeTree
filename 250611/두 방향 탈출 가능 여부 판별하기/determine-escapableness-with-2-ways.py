2ec50ae0b7d8444610d79c0e62675956982fa083
catnote0 <catnote1@gmail.com>
2025-06-11 21:39:33 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 250611/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250611/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,12 +1,13 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-
+visited = [[False] * m for _ in range(n)]
 def search(x, y):
+    visited[x][y] = True
     if x == n - 1 and y == m - 1:
         print(1)
         exit(0)
-    if 0 <= x < n and 0 <= y + 1 < m and grid[x][y + 1]: search(x, y + 1)
-    if 0 <= x + 1 < n and 0 <= y < m and grid[x + 1][y]: search(x + 1, y)
+    if 0 <= x < n and 0 <= y + 1 < m and grid[x][y + 1] and not visited[x][y + 1]: search(x, y + 1)
+    if 0 <= x + 1 < n and 0 <= y < m and grid[x + 1][y] and not visited[x + 1][y]: search(x + 1, y)
 
 search(0, 0)
 print(0)
