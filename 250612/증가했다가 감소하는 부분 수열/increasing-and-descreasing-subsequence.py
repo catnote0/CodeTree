79c48723e9cd3545f48f0549b24b129d0511ff9d
catnote0 <catnote1@gmail.com>
2025-06-12 03:32:34 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 73ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250612/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -0,0 +1,20 @@
+n = int(input())
+sequence = list(map(int, input().split()))
+dp = [1] + [0] * (n - 1)
+dp_rev = [0] * (n - 1) + [1]
+
+for i in range(n):
+    max_value = 0
+    for j in range(i):
+        if sequence[j] < sequence[i]: max_value = max(max_value, dp[j])
+    dp[i] = max_value + 1
+
+for i in range(n - 1, -1, -1):
+    max_value = 0
+    for j in range(n - 1, i, -1):
+        if sequence[j] < sequence[i]: max_value = max(max_value, dp[j])
+    dp_rev[i] = max_value + 1
+
+result = 0
+for i in range(n): result = max(result, dp[i] + dp_rev[i] - 1)
+print(result)
