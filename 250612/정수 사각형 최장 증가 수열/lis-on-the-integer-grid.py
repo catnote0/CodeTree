0b0ca405318c824bf06de88262f233328794a889
catnote0 <catnote1@gmail.com>
2025-06-12 02:32:03 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 215ms, Memory: 28MB, Status: Passed - Codetree

--- 250612/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 250612/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,32 +1,25 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 drc = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-Queue = []
+dfs_grid = [[-1] * n for _ in range(n)]
 result = 0
 
-def BFS():
-    global Queue
-    T = 0
-    while True:
-        T += 1
-        tmpQueue = []
-        for x, y in Queue:
-            for dx, dy in drc:
-                nx, ny = x + dx, y + dy
-                if nx < 0 or n <= nx or ny < 0 or n <= ny: continue
-                if grid[nx][ny] < grid[x][y]: tmpQueue.append([nx, ny])
-        if not tmpQueue: return T
-        Queue = tmpQueue
+def dfs(x, y):
+    if dfs_grid[x][y] != -1: return dfs_grid[x][y]
+    max_dist = 0
+    for dx, dy in drc:
+        nx, ny = x + dx, y + dy
+        if nx < 0 or n <= nx or ny < 0 or n <= ny: continue
+        if grid[nx][ny] >= grid[x][y]: continue
+        max_dist = max(max_dist, dfs(nx, ny))
+    dfs_grid[x][y] = max_dist + 1
+    return dfs_grid[x][y]
 
-for x in range(n):
-    for y in range(n):
-        highest = True
-        for dx, dy in drc:
-            nx, ny = x + dx, y + dy
-            if nx < 0 or n <= nx or ny < 0 or n <= ny: continue
-            if grid[nx][ny] > grid[x][y]:
-                highest = False
-                break
-        if highest: Queue.append([x, y])
+for i in range(n):
+    for j in range(n):
+        if dfs_grid[i][j] == -1: result = max(result, dfs(i, j))
 
-print(BFS())
+print(result)
