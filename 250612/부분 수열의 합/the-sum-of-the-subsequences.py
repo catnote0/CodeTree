75d0d9031ea774db937adf64b777365b3c6d82ac
catnote0 <catnote1@gmail.com>
2025-06-12 23:40:07 +0900

    Title: 부분 수열의 합, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250612/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+
+DP = [0] + [0x7FFFFFFF] * M
+
+for c in coin:
+    for i in range(M - c, -1, -1):
+        DP[i + c] = min(DP[i + c], DP[i] + 1)
+
+print("No" if DP[M] == 0x7FFFFFFF else "Yes")
