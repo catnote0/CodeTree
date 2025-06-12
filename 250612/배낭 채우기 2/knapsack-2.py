c97972d9e1d219bbbb086213701cfe2de6dec6c0
catnote0 <catnote1@gmail.com>
2025-06-12 23:59:01 +0900

    Title: 배낭 채우기 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250612/배낭 채우기 2/knapsack-2.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+w, v = zip(*[tuple(map(int, input().split())) for _ in range(N)])
+w, v = list(w), list(v)
+
+DP = [0] + [-0x7FFFFFFF] * M
+
+for i in range(N):
+    for j in range(M - w[i] + 1):
+        DP[j + w[i]] = max(DP[j + w[i]], DP[j] + v[i])
+
+print(max(DP))
