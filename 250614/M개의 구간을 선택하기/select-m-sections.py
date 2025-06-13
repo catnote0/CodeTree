7e306f4e66b5e2e986fbb0e89c2eba1230002176
catnote0 <catnote1@gmail.com>
2025-06-14 04:26:51 +0900

    Title: M개의 구간을 선택하기, Time: 71ms, Memory: 19MB, Status: Passed - Codetree

--- 250614/M개의 구간을 선택하기/select-m-sections.py
+++ 250614/M개의 구간을 선택하기/select-m-sections.py
@@ -7,7 +7,9 @@ DP[0][0] = 0
 
 for i in range(1, N + 1):
     for j in range(2 * M + 1):
-        if j % 2 == 0: DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j])
+        if j % 2 == 0:
+            if j > 0: DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j])
+            else: DP[i][j] = DP[i - 1][j]
         else: DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j]) + numbers[i]
 
 print(max(DP[N][2 * M - 1], DP[N][2 * M]))
