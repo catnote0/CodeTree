87f96f8356d45df4cbdf3e8cb182bb5547ef0ff4
catnote0 <catnote1@gmail.com>
2025-09-15 22:58:00 +0900

    Title: 원소의 합이 0, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250915/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,20 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+AB = {}
+CD = {}
+
+for a in A:
+    for b in B: AB[a + b] = AB[a + b] if (a + b) in AB else 1
+
+for c in C:
+    for d in D: CD[c + d] = CD[c + d] if (c + d) in CD else 1
+
+Result = 0
+for key in AB.keys():
+    if -key in CD: Result += AB[key] * CD[-key]
+
+print(Result)
