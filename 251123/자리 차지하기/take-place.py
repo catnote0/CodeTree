edf55acfebf751e44fa6246af4ca2596ef916d27
catnote0 <catnote1@gmail.com>
2025-11-23 22:20:34 +0900

    Title: 자리 차지하기, Time: 94ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251123/자리 차지하기/take-place.py
@@ -0,0 +1,13 @@
+from sortedcontainers import SortedSet
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+S = SortedSet(range(1, m + 1))
+
+for i, a_i in enumerate(a):
+    opt = S.bisect_right(a_i) - 1
+    if opt < 0:
+        print(i)
+        exit(0)
+    S.remove(S[opt])
