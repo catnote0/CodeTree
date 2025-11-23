be738ccc2935e20891b2accfb2d2d4163249b28a
catnote0 <catnote1@gmail.com>
2025-11-23 22:25:06 +0900

    Title: 차이가 가장 작은 수, Time: 99ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251123/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedSet
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+S = SortedSet(arr)
+
+Result = 0x7FFFFFFF
+for num in arr:
+    opt = S.bisect_left(num + m)
+    if opt == len(arr): continue
+    Result = min(Result, S[opt] - num)
+
+if Result == 0x7FFFFFFF: print(-1)
+else: print(Result)
