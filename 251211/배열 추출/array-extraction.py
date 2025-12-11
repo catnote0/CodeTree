ccee1f88489020549172b3f96ee59f05de46e026
catnote0 <catnote1@gmail.com>
2025-12-11 20:47:33 +0900

    Title: 배열 추출, Time: 253ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 251211/배열 추출/array-extraction.py
@@ -0,0 +1,12 @@
+import heapq
+
+n = int(input())
+x = [int(input()) for _ in range(n)]
+
+queue = []
+
+for a in x:
+    if a == 0:
+        if not queue: print(0)
+        else: print(-heapq.heappop(queue))
+    else: heapq.heappush(queue, -a)
