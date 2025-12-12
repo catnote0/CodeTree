ef54c099f5bc6f5bf39830f14b4f369d88013ae9
catnote0 <catnote1@gmail.com>
2025-12-12 19:28:16 +0900

    Title: 이상한 폭탄, Time: 127ms, Memory: 25MB, Status: Passed - Codetree

--- 251212/이상한 폭탄/strange-bomb.py
+++ 251212/이상한 폭탄/strange-bomb.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
-Count = [0] * 1100000
+Dict = {}
 
 Result = -1
 for i in range(n):
-    if i > k: Count[arr[i - k - 1]] -= 1
-    if Count[arr[i]]: Result = max(Result, arr[i])
-    Count[arr[i]] += 1
+    if i > k: Dict[arr[i - k - 1]] -= 1
+    if arr[i] in Dict and Dict[arr[i]] > 0: Result = max(Result, arr[i])
+    Dict[arr[i]] = Dict[arr[i]] + 1 if arr[i] in Dict else 1
 
 print(Result)
