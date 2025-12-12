b1db646dcaafd7ab2331c6d959d6a58c05070c75
catnote0 <catnote1@gmail.com>
2025-12-13 02:57:48 +0900

    Title: 수 채우기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251213/수 채우기/fill-in-number.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(100000)
+
+inf = 0x7FFFFFFF
+n = int(input())
+ans = [inf, inf, 1, inf, 2, 1] + [inf] * n
+
+def solve(n):
+    if n < 0: return inf
+    if ans[n] < inf: return ans[n]
+    ans[n] = min(solve(n - 2), solve(n - 5)) + 1
+    return ans[n]
+
+
+print(solve(n))
