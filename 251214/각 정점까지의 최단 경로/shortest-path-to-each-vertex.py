00e8d6a717dee1d4a471c41c1d54bc9163d670d3
catnote0 <catnote1@gmail.com>
2025-12-14 16:11:11 +0900

    Title: 각 정점까지의 최단 경로, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251214/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,32 @@
+import heapq
+
+n, m = map(int, input().split())
+k = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+edges_reverse = [(e, s, v) for (s, e, v) in edges]
+edges += edges_reverse
+
+inf = 10**10
+edges.sort()
+edge_info = [-1] * (n + 2)
+for i, (s, _, _) in enumerate(edges): edge_info[s] = i if edge_info[s] == -1 else edge_info[s]
+edge_info[n + 1] = m
+for i in range(n, 0, -1): edge_info[i] = edge_info[i + (1 if edge_info[i] == -1 else 0)]
+
+Result = [inf] * (n + 1)
+
+def dijkstra(start):
+    Result[start] = 0
+    pq = []
+    for i in range(1, n + 1): heapq.heappush(pq, (Result[i], i))
+    while pq:
+        dist, pos = heapq.heappop(pq)
+        if dist > Result[pos]: continue
+        for i in range(edge_info[pos], edge_info[pos + 1]):
+            s, e, v = edges[i]
+            if dist + v < Result[e]:
+                Result[e] = dist + v
+                heapq.heappush(pq, (Result[e], e))
+    for i in range(1, n + 1): print(-1 if Result[i] == inf else Result[i])
+
+dijkstra(k)
